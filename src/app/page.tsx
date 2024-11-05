2dabd2e432e71bc843fb02aa1eb5d68c4c981d6c
CodeeBy <khamidullayevammoriddin@gmail.com>
2024-11-05 13:15:57 +0500

    FIX: Image not showing bug

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import demo from './images/demo.png'
 
 export default function Home() {
   return (
@@ -8,12 +9,12 @@ export default function Home() {
       <div className="flex flex-wrap justify-center gap-[23px]">
 
         <div className="border-[4px] border-black w-auto">
-          <Image width={500} height={600} className="w-[500px] border-b-[4px] border-black" alt="static demo" src={'/images/demo.png'} />
+          <Image width={500} height={600} className="w-[500px] border-b-[4px] border-black" alt="static demo" src={demo} />
           <a href="https://brutalism-portfolio.vercel.app/" target="_blank" className="flex py-[10px] px-[30px] text-[23px] font-medium">Static Demo</a>
         </div>
 
         <div className="border-[4px] border-black w-auto">
-          <Image width={500} height={600} className="w-[500px] border-b-[4px] border-black" alt="static demo" src={'/images/demo.png'} />
+          <Image width={500} height={600} className="w-[500px] border-b-[4px] border-black" alt="Aos animation demo" src={demo} />
           <a href="https://brutalism-portfolio.vercel.app/" target="_blank" className="flex py-[10px] px-[30px] text-[23px] font-medium">AOS Animation Demo</a>
         </div>
       </div>
